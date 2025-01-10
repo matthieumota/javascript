bea8017422ccdbba71f1e00762f9074992b7d21e
Matthieu Mota <matthieumota@gmail.com>
2025-01-10 16:12:08 +0100

    end

--- jour3/src/main.js
+++ jour3/src/main.js
@@ -18,3 +18,23 @@ loadTodos() // Le load est asynchrone
 setupProducts()
 
 console.log('après')
+
+const mediaQuery = window.matchMedia('(max-width: 768px)')
+
+function handleTabletChange(e) {
+    if (e.matches) {
+        // L'écran est plus petit que 768px
+        console.log('Écran mobile ou tablette')
+    } else {
+        // L'écran est plus grand que 768px
+        console.log('Écran de bureau')
+    }
+}
+
+mediaQuery.addEventListener('change', handleTabletChange)
+handleTabletChange(mediaQuery)
+
+// Plus performant que...
+window.addEventListener('resize', () => {
+    console.log('resize')
+})
