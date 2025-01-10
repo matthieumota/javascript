1d1dbe0d936ed1507bb336975613df83fb579943
Matthieu Mota <matthieumota@gmail.com>
2025-01-10 09:22:41 +0100

    fix exo

--- jour2/form.js
+++ jour2/form.js
@@ -54,7 +54,11 @@ const errorElement = document.getElementById('error')
 
 passwordField.addEventListener('input', () => {
     const password = passwordField.value
-    errorElement.textContent = ''
+
+    if (password.length <= 0) {
+        errorElement.textContent = ''
+        return
+    }
 
     const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     if (!numbers.some(n => password.includes(n))) {
